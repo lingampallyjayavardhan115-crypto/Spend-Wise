f573903cfc775f6d64f890bee2cceba49654a96e
Jayavardhan Lingampally <lingampallyjayavardhan115@gmail.com>
2026-06-27 17:50:16 +0530

    Fix for Render deployment

--- api/index.py
+++ api/index.py
@@ -231,5 +231,7 @@ def delete_budget(user_id, bid):
         return jsonify({'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
 
